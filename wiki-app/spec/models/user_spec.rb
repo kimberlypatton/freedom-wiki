b63d17b927f8397b11f62e1ea1e46999c666b6b0
CJ Jessett <cjessett@gmail.com>
2016-08-26 15:37:21 -0500

    passing user model specs

--- wiki-app/spec/models/user_spec.rb
+++ wiki-app/spec/models/user_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
 describe User do
-  context 'validations' do
-    let(:user) { User.create!(first_name: 'bob', email: 'bob@bob.com') }
+  let(:user) { User.create!(first_name: 'bob',
+                            email: 'bob@bob.com',
+                            last_name: 'doe',
+                            password: 'test'
+                            )}
 
+  context 'validations' do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
     it { should have_secure_password }
+  end
+
+  context '#fullname' do
+    it 'returns the users fullname' do
+      expect(user.fullname).to eq 'Bob Doe'
+    end
   end
 end
